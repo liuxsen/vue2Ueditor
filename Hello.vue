21599aae3cf8a2a7ad92c7a039df071459fbd016
liuxsen <948771968@qq.com>
2017-05-24 12:00:03 +0800

    '添加vueUeditor组件'

---
+++ Hello.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="edit-area">
+      <ueditor v-bind:value=defaultMsg v-bind:config=config v-on:input="input" v-on:ready="ready"></ueditor>
+    </div>
+</template>
+
+<script>
+import ueditor from './vueUditor'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      defaultMsg: '初始文本',
+      config: {
+        initialFrameWidth: null,
+        initialFrameHeight: 200,
+      }
+    }
+  },
+  components: {
+    ueditor
+  },
+  methods: {
+    ready(editorInstance){
+      console.log(editorInstance)
+    },
+    input(obj){
+        console.log(obj);
+    }
+  },
+  mounted(){
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
